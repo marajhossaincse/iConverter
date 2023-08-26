f05ae85c13e92cd6b401fde14c4e09485bbd4131
Maraj Hossain <marajhossaincse@gmail.com>
2023-08-26 20:24:51 +0600

    add logic for more convertions, fixed padding, perfected convertion logic.

--- iConverter/ContentView.swift
+++ iConverter/ContentView.swift
@@ -8,22 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    var temperatureUnits = ["-", "Celsius", "Fahrenheit", "Kelvin"]
     @FocusState private var amountIsFocused: Bool
 
     @State var inputValue = 0.0
-    @State var selectedInputUnit: String
-    @State var selectedOutputUnit: String
+    @State var selectedInputUnit: String = ""
+    @State var selectedOutputUnit: String = ""
     var outputValue: Double {
         var amount: Double = inputValue
 
         if selectedInputUnit.lowercased() == "celsius" {
             if selectedOutputUnit.lowercased() == "fahrenheit" {
-                amount = (inputValue * (9.0 / 5.0) + 32.0)
+                amount = (inputValue * (9.0/5.0) + 32.0)
 
             } else if selectedOutputUnit.lowercased() == "kelvin" {
                 print("out unit ", selectedOutputUnit)
                 amount = inputValue + 273.15
+            }
+
+        } else if selectedInputUnit.lowercased() == "fahrenheit" {
+            if selectedOutputUnit.lowercased() == "celsius" {
+                amount = (inputValue - 32.0) * (5.0/9.0)
+            } else if selectedOutputUnit.lowercased() == "kelvin" {
+                amount = ((inputValue - 32.0) * (5.0/9.0)) + 273.15
+            }
+
+        } else if selectedInputUnit.lowercased() == "kelvin" {
+            if selectedOutputUnit.lowercased() == "celsius" {
+                amount = (inputValue - 273.15)
+            } else {
+                amount = (inputValue - 273.15) * 9/5 + 32
             }
         }
         return amount
@@ -32,16 +46,17 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
                     Section {
-                        Picker("Input unit",
+                        Picker("Select input unit:",
                                selection: $selectedInputUnit) {
                             ForEach(temperatureUnits, id: \.self) { unit in
                                 Text(unit)
                             }
                         }
-                        .padding(.vertical, 7)
+                        .padding(.vertical, 5)
                     }
+                    .font(.headline)
 
                     Section {
                         TextField("", value: $inputValue, format: .number)
@@ -50,39 +65,42 @@ struct ContentView: View {
                             .cornerRadius(10)
                             .focused($amountIsFocused)
                     } header: {
-                        Text("Input amount")
+                        Text("Amount to convert: ")
+                            .font(.body)
+                            .padding(.bottom, 5)
                     }
 
                     Section {
-                        Picker("Output unit",
+                        Picker("Select output unit:",
                                selection: $selectedOutputUnit) {
                             ForEach(temperatureUnits, id: \.self) { unit in
                                 Text(unit)
                             }
                         }
-                        .padding(.vertical, 7)
+                        .font(.headline)
+                        .padding(.vertical, 5)
                     }
 
                     Section {
-                        Text("\(outputValue)")
+                        Text(String(format: "%.2f", outputValue))
                             .padding()
-                            .frame(maxWidth: .infinity)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                             .background(Color.black.opacity(0.3))
                             .cornerRadius(10)
                     } header: {
                         Text("Output amount: ")
+                            .font(.body)
+                            .padding(.bottom, 5)
                     }
                 }
-                .padding()
             }
             .navigationTitle("iConverter")
         }
     }
 }
 
-//
-// struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-// }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
